d239b78664a25d58e4adca188aee1b2251bfd9a6
BryhanS <bryhansalazarpacsi@gmail.com>
2022-10-01 02:01:12 -0500

    avance de header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./public/index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        'primary':'#f9f9f9',
+        'secondary': '#ebebeb',
+        'tertiary': '#e1692b'
+      })
+    },
+
   },
   plugins: [],
 }
+
